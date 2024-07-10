a06147a33e08fc27270577d2a6a0c2b6ad296be7
peterscarborough <phone.peter.toronto@gmail.com>
2024-07-10 08:29:09 -0400

    Update route.js

--- app/quotes/random/route.js
+++ app/quotes/random/route.js
@@ -3,7 +3,7 @@ import data from 'data/quotes.json';
 
 export const dynamic = 'force-dynamic'; // Otherwise, Next.js will cache this handler's output
 
-const dataSource = 'https://en.wikipedia.org/wiki/AFI%27s_100_Years...100_Movie_Quotes';
+const dataSource = 'https://sandersonmonument.ca/';
 
 export async function GET() {
     const randomId = Math.floor(Math.random() * data.length);
